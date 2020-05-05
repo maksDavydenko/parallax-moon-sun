27e4a25e9f2eee09c08722d9ea44a00df51496f0
max davydenko <maksim.davydenko@gmail.com>
2020-05-05 16:38:49 +0300

    add srcset img

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ const road = document.querySelector('.road');
 const text = document.querySelector('.text');
 const sun = document.querySelector('.sun');
 const textDay = document.querySelector('.text_day')
-const deviceHeight = document.body.clientHeight;
-
+const deviceWidth = document.body.clientWidth;
+console.log(deviceWidth);
 window.addEventListener('scroll', ()=>{
     let value = window.scrollY;
 
@@ -16,7 +16,7 @@ window.addEventListener('scroll', ()=>{
     mountain.style.top = -value * 0.15 + 'px';
     road.style.top = value * 0.15 + 'px';
     text.style.top = value * 1 + 'px';
-    sun.style.right = -value * 0.5 + 'px';
+    sun.style.left =  value * 0.5 + 'px';
     sun.style.top = -value * 0.3 + 'px';
     mountainDay.style.top = (-value * 0.15) + 'px';
     textDay.style.top = -value * 0.2 + 'px'
